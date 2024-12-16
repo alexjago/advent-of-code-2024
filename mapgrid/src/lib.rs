5a8c7d81ec56d6c9f498f702b29998b66af7032c
Alex Jago <4475543+alexjago@users.noreply.github.com>
2024-12-16 22:40:35 +1000

    mapgrid: re-enable BTree variants, also we might be able to use the good syntax again?

--- mapgrid/src/lib.rs
+++ mapgrid/src/lib.rs
@@ -1,10 +1,8 @@
-use std::cmp::{Eq, Ord, PartialEq, PartialOrd};
 use std::{
     collections::{BTreeMap, BTreeSet, HashMap, HashSet},
-    ops::{Add, RangeInclusive},
+    ops::RangeInclusive,
 };
 
-// #[derive(From, Into, PartialOrd, PartialEq, Ord, Eq)]
 pub type Coord = [isize; 2];
 
 /// A Grid trait
@@ -15,22 +13,50 @@ pub trait Grid<V = char> {
 
     fn visualise(&self) -> String;
 }
-/*
-impl<V> Grid<V> for BTreeMap<Coord, V> {
+
+impl<V> Grid<V> for BTreeMap<Coord, V>
+where
+    V: ToString,
+{
     fn from_str_with<F: Fn(char) -> Option<V>>(input: &str, f: F) -> BTreeMap<Coord, V> {
         let mut out = BTreeMap::new();
 
         for (row, s) in input.lines().enumerate() {
             for (col, cha) in s.chars().enumerate() {
                 if let Some(v) = f(cha) {
-                    out.insert([row as isize, col as isize], v);
+                    out.insert([col as isize, row as isize], v);
                 }
             }
         }
         out
     }
-}
+    fn bounds(&self) -> [RangeInclusive<isize>; 2] {
+        let xmax = self.iter().map(|(v, _)| v[0]).max().unwrap_or(0);
+        let ymax = self.iter().map(|(v, _)| v[1]).max().unwrap_or(0);
+        let xmin = self.iter().map(|(v, _)| v[0]).min().unwrap_or(0);
+        let ymin = self.iter().map(|(v, _)| v[1]).min().unwrap_or(0);
 
+        [xmin..=xmax, ymin..=ymax]
+    }
+
+    /// Visualise the grid. Note that only the first character is used.
+    fn visualise(&self) -> String {
+        let mut out = String::new();
+        let [xs, ys] = <BTreeMap<Coord, V> as Grid<V>>::bounds(self);
+
+        for y in ys {
+            for x in xs.clone() {
+                if let Some(v) = self.get(&[x, y]) {
+                    out.push(v.to_string().chars().nth(0).unwrap_or('#'));
+                } else {
+                    out.push('.');
+                }
+            }
+            out.push('\n');
+        }
+        out
+    }
+}
 impl<V> Grid<V> for BTreeSet<Coord> {
     /// Note: the value of V is disregarded
     fn from_str_with<F: Fn(char) -> Option<V>>(input: &str, f: F) -> BTreeSet<Coord> {
@@ -39,29 +65,38 @@ impl<V> Grid<V> for BTreeSet<Coord> {
         for (row, s) in input.lines().enumerate() {
             for (col, cha) in s.chars().enumerate() {
                 if f(cha).is_some() {
-                    out.insert([row as isize, col as isize]);
+                    out.insert([col as isize, row as isize]);
                 }
             }
         }
         out
     }
-}
+    fn bounds(&self) -> [RangeInclusive<isize>; 2] {
+        let xmax = self.iter().map(|v| v[0]).max().unwrap_or(0);
+        let ymax = self.iter().map(|v| v[1]).max().unwrap_or(0);
+        let xmin = self.iter().map(|v| v[0]).min().unwrap_or(0);
+        let ymin = self.iter().map(|v| v[1]).min().unwrap_or(0);
 
-impl<V> Grid<V> for HashMap<Coord, V> {
-    fn from_str_with<F: Fn(char) -> Option<V>>(input: &str, f: F) -> HashMap<Coord, V> {
-        let mut out = HashMap::new();
+        [xmin..=xmax, ymin..=ymax]
+    }
 
-        for (row, s) in input.lines().enumerate() {
-            for (col, cha) in s.chars().enumerate() {
-                if let Some(v) = f(cha) {
-                    out.insert([row as isize, col as isize], v);
+    fn visualise(&self) -> String {
+        let mut out = String::new();
+        let [xs, ys] = <BTreeSet<[isize; 2]> as Grid>::bounds(self);
+
+        for y in ys {
+            for x in xs.clone() {
+                if self.contains(&[x, y]) {
+                    out.push('#');
+                } else {
+                    out.push('.');
                 }
             }
+            out.push('\n');
         }
         out
     }
 }
-*/
 
 impl<V> Grid<V> for HashSet<Coord> {
     /// Note: the value of V is disregarded
@@ -162,4 +197,48 @@ pub fn subtract(a: Coord, b: Coord) -> Coord {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const ROUND_TRIP: &str = r"#####
+..#..
+..#..
+#.#..
+.#...";
+
+    #[test]
+    fn round_trip() {
+        let hs = <HashSet<Coord> as Grid>::from_str_with(ROUND_TRIP, |c| match c {
+            '.' => None,
+            e => Some(e),
+        });
+        let bs = <BTreeSet<Coord> as Grid>::from_str_with(ROUND_TRIP, |c| match c {
+            '.' => None,
+            e => Some(e),
+        });
+        let hm = <HashMap<Coord, char> as Grid>::from_str_with(ROUND_TRIP, |c| match c {
+            '.' => None,
+            e => Some(e),
+        });
+        let bm = <BTreeMap<Coord, char> as Grid>::from_str_with(ROUND_TRIP, |c| match c {
+            '.' => None,
+            e => Some(e),
+        });
+
+        assert_eq!(
+            ROUND_TRIP,
+            <HashSet<Coord> as Grid>::visualise(&hs).as_str().trim()
+        );
+        assert_eq!(
+            ROUND_TRIP,
+            <BTreeSet<Coord> as Grid>::visualise(&bs).as_str().trim()
+        );
+        assert_eq!(
+            ROUND_TRIP,
+            <HashMap<Coord, char> as Grid>::visualise(&hm)
+                .as_str()
+                .trim()
+        );
+
+        // the future is here
+        assert_eq!(ROUND_TRIP, bm.visualise().as_str().trim());
+    }
 }
